94b15366a60b98ad48e9c1326a8be3db1fdc46e9
Ashish Jain <ashishjain0338@gmail.com>
2020-09-23 12:35:08 +0530

    Genralised some fxn of Task#2 and added the basic structure of profile.html

--- Task#2/Q1.py
+++ Task#2/Q1.py
@@ -20,16 +20,26 @@ def DictOrder(l1):
     l1.sort()
     return l1
 
-def FindLongest(s1,s2,s3):
-    out = max(s1 , s2)
-    out = max(out , s3)
+def FindLongest(list1):
+
+    out = []
+    if(len(list1)):
+        max_length = len(list1[0])
+        for x in list1:
+            if(len(x) > max_length):
+                max_length = len(x)
+                out = []
+                out.append(x)
+            elif(len(x) == max_length):
+                out.append(x)
     return out
 
 
 list1 = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
-
+list2 = ['Supercalifragilisticexpialidocious','Honorificabilitudinitatibus','Bababadalgharaghtakamminarronnkonn']
+# list3 = ['a','b','c','d']
 print('There are ',CountLetters('Supercalifragilisticexpialidocious'),' letters in Supercalifragilisticexpialidocious')
 print("'Supercalifragilisticexpialidocious' contain 'ice' as a substring-->",PatternMatch('Supercalifragilisticexpialidocious','ice'))
-print('Longest word is -->',FindLongest('Supercalifragilisticexpialidocious','Honorificabilitudinitatibus','Bababadalgharaghtakamminarronnkonn'))
+print('Longest word is -->',FindLongest(list2))
 print("First word acc to dict order --> ",DictOrder(list1)[0])
 print("Last word acc to dict order --> ",DictOrder(list1)[-1])
